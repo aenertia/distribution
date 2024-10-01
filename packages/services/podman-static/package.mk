16664f87a2721e11cd7a59c1116fe3722d760a11
Joel Pauling (aenertia) <aenertia@aenertia.net>
2024-10-02 09:38:44 +1300

    Adding btrfs-progs and podman-static packages

--- packages/services/podman-static/package.mk
+++ packages/services/podman-static/package.mk
@@ -10,3 +10,19 @@ PKG_URL="${PKG_SITE}/releases/download/v${PKG_VERSION}/podman-linux-arm64.tar.gz
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Arm64 Satically Linked Podman runc and Dependancies"
 PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+ # Systemd and related units and configs
+ mkdir -p ${INSTALL}/etc/containers
+  cp -P etc/containers/* ${INSTALL}/etc/containers
+ mkdir -p ${INSTALL}/usr/lib/systemd/system
+  cp -P usr/lib/systemd/system/* ${INSTALL}/usr/lib/systemd/system
+ mkdir -p ${INSTALL}/usr/lib/systemd/user 
+  cp -P usr/lib/systemd/user/* ${INSTALL}/usr/lib/systemd/user
+ 
+ # Binaries
+ mkdir -p ${INSTALL}/usr/local/bin
+  cp -P usr/local/bin/* ${INSTALL}/usr/local/bin
+ mkdir -p ${INSTALL}/usr/local/lib/podman
+  cp -P usr/local/lib/podman/* ${INSTALL}/usr/local/lib/podman
+}
