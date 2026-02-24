c588ad46729ff564d88c80ec490d73b187fbaa20
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-02-24 21:14:12 +1300

    (wifi) package fixes

--- projects/ROCKNIX/packages/linux-drivers/sv6160/package.mk
+++ projects/ROCKNIX/packages/linux-drivers/sv6160/package.mk
@@ -6,21 +6,26 @@ PKG_SITE="https://codeberg.org/aenertia/sv6160-rk915"
 PKG_URL="https://codeberg.org/aenertia/sv6160-rk915.git"
 PKG_DEPENDS_TARGET="linux"
 
+# Required by ROCKNIX for out-of-tree kernel modules
+PKG_TOOLCHAIN="manual"
+PKG_IS_KERNEL_PKG="yes"
+
 make_target() {
-    # Build the driver out-of-tree using the compiled ROCKNIX kernel
+    # Use the ROCKNIX kernel_make wrapper
     cd ${PKG_BUILD}/drivers/net/wireless/seekwave/sv6160
-    $(MAKE) -C "${LINUX_OUT}" M="$(pwd)" ARCH="${TARGET_KERNEL_ARCH}" CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" modules
+    kernel_make KDIR=$(kernel_path) M=$(pwd) modules
 }
 
 makeinstall_target() {
-    # 1. Install the compiled kernel module
+    # 1. Install the compiled kernel module using the ROCKNIX path helper
     cd ${PKG_BUILD}/drivers/net/wireless/seekwave/sv6160
-    $(MAKE) -C "${LINUX_OUT}" M="$(pwd)" ARCH="${TARGET_KERNEL_ARCH}" INSTALL_MOD_PATH="${INSTALL}/usr" modules_install
-    
+    mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+    cp sv6160.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}/
+
     # 2. Install the latency optimization config
     mkdir -p "${INSTALL}/usr/lib/modprobe.d"
     cp -av ${PKG_BUILD}/conf/sv6160.conf "${INSTALL}/usr/lib/modprobe.d/"
-    
+
     # 3. Install the firmware blobs directly to the rootfs
     mkdir -p "${INSTALL}/usr/lib/firmware"
     cp -av ${PKG_BUILD}/firmware/*.bin "${INSTALL}/usr/lib/firmware/"
