fe4d6e29679ec38f91f408820b20db878bd1dac1
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-03-16 08:00:49 +1300

    kernel-firmware: bump 20250509 → 20260309
    10 months of upstream firmware updates. Includes updated blobs
    for WiFi (iwlwifi, ath11k, rtw89), GPU (amdgpu, i915), and
    various SoC firmware.

--- packages/linux-firmware/kernel-firmware/package.mk
+++ packages/linux-firmware/kernel-firmware/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kernel-firmware"
-PKG_VERSION="20250509"
-PKG_SHA256="f2c60d66f226a28130cb5643e6e544d3229673460e127c91ba03f1080cbd703e"
+PKG_VERSION="20260309"
+PKG_SHA256="c74cc6f562b58ad5bc6b2b00a61abc29c9e49e06126e7ba34fbca9928e07a96c"
 PKG_LICENSE="other"
 PKG_SITE="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/"
 PKG_URL="https://cdn.kernel.org/pub/linux/kernel/firmware/linux-firmware-${PKG_VERSION}.tar.xz"
