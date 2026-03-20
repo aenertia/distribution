d237eea5e222f81f78c7c508a776c0b40ec9da58
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-03-20 16:43:36 +1300

    ES: bump to 69c85b32b (storage profile UI)
    Picks up storage profile display in System Information and
    profile selector + Use Update Defaults in System Settings.

--- projects/ROCKNIX/packages/sysutils/busybox/package.mk
+++ projects/ROCKNIX/packages/sysutils/busybox/package.mk
@@ -212,7 +212,9 @@ makeinstall_init() {
 
   # libcrypt needed by CONFIG_LOGIN (recovery serial console)
   mkdir -p ${INSTALL}/usr/lib
-    cp -PR ${TOOLCHAIN}/${TARGET_NAME}/sysroot/usr/lib/libcrypt.so* ${INSTALL}/usr/lib
+    cp ${TOOLCHAIN}/${TARGET_NAME}/sysroot/usr/lib/libcrypt.so.2.0.0 ${INSTALL}/usr/lib/
+    ln -sf libcrypt.so.2.0.0 ${INSTALL}/usr/lib/libcrypt.so.2
+    ln -sf libcrypt.so.2.0.0 ${INSTALL}/usr/lib/libcrypt.so
 
   mkdir -p ${INSTALL}/etc
     touch ${INSTALL}/etc/fstab
