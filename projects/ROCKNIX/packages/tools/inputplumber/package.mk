42655d7190266214062ad1f97f7892a47773efc7
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-03-21 11:00:12 +1300

    inputplumber: temporary switch to ICM42607-patched binary
    Use binary-patched v0.75.2 from our fork (aenertia/InputPlumber@20570a4)
    with ICM42607P IIO matching support. The upstream binary doesn't match
    icm42607p-gyro/accel IIO device names.
    
    TEMPORARY: revert to upstream when feat/icm42607-iio-support PR is
    merged and a new InputPlumber release includes the fix.

--- projects/ROCKNIX/packages/tools/inputplumber/package.mk
+++ projects/ROCKNIX/packages/tools/inputplumber/package.mk
@@ -2,13 +2,22 @@
 # Copyright (C) 2025 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="inputplumber"
-PKG_VERSION="v0.75.2"
+# TEMPORARY: using binary-patched v0.75.2 from our fork with ICM42607 IIO support.
+# Revert to upstream release once https://github.com/ShadowBlip/InputPlumber/pull/XXX
+# (feat/icm42607-iio-support) is merged and a new release is cut.
+PKG_VERSION="20570a4"
 PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/ShadowBlip/InputPlumber"
-PKG_URL="https://github.com/ShadowBlip/InputPlumber/releases/download/${PKG_VERSION}/inputplumber-aarch64.tar.gz"
+PKG_SITE="https://github.com/aenertia/InputPlumber"
+PKG_URL="https://raw.githubusercontent.com/aenertia/InputPlumber/${PKG_VERSION}/prebuilt/inputplumber-aarch64"
 PKG_DEPENDS_TARGET="toolchain systemd libevdev libiio polkit"
 PKG_LONGDESC="Open source input router and remapper daemon for Linux"
 PKG_TOOLCHAIN="manual"
+
+unpack() {
+  mkdir -p ${PKG_BUILD}/usr/bin
+  cp ${SOURCES}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.inputplumber-aarch64 ${PKG_BUILD}/usr/bin/inputplumber
+  chmod +x ${PKG_BUILD}/usr/bin/inputplumber
+}
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr
