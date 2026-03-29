d35fb822a878653149abecea4d95bbd514a47693
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-03-30 01:53:51 +1300

    amiberry: update to v8.1.2 (SDL3 + ImGui migration)
    Major update from SDL2/Guisan Makefile build to SDL3/ImGui CMake build:
    - PKG_VERSION bumped to v8.1.2 (37896181)
    - PKG_SITE changed to BlitterStudio/amiberry (repo migrated)
    - Switched from make to cmake toolchain
    - SDL2/SDL2_image/SDL2_ttf replaced with SDL3/SDL3_image
    - Added curl, nlohmann-json dependencies (new for v8)
    - Removed old Makefile patches (01-platform, 02-gcc15, opengl/001-fix-sdl)
    - Gate GLES/OpenGL/Vulkan on device capabilities
    - Build NOT yet verified — OOM kill interrupted build process

--- projects/ROCKNIX/packages/emulators/standalone/amiberry/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/amiberry/package.mk
@@ -1,54 +1,80 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+# Copyright (C) 2026 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="amiberry"
 PKG_ARCH="aarch64"
-PKG_VERSION="5c54536997c0039aaa72bb0552cefdc3c967ad8d"
+PKG_VERSION="37896181fb36ec3ce39322827dabdbf311eea716" # v8.1.2
 PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/midwan/amiberry"
+PKG_SITE="https://github.com/BlitterStudio/amiberry"
 PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain linux glibc bzip2 zlib SDL2 SDL2_image SDL2_ttf capsimg freetype libxml2 flac libogg mpg123 libpng libmpeg2 libserialport"
-PKG_LONGDESC="Amiberry is an optimized Amiga emulator for ARM-based boards."
+PKG_DEPENDS_TARGET="toolchain linux glibc bzip2 zlib SDL3 SDL3_image capsimg freetype libxml2 flac libogg mpg123 libpng libmpeg2 libserialport curl nlohmann-json"
+PKG_LONGDESC="Amiberry is an optimized Amiga emulator (v8.x with SDL3 + ImGui)."
 GET_HANDLER_SUPPORT="git"
-PKG_TOOLCHAIN="make"
-PKG_GIT_CLONE_BRANCH="master"
-PKG_PATCH_DIRS+=" ${DEVICE}"
+PKG_TOOLCHAIN="cmake"
 
-if [ ! "${OPENGL}" = "no" ]; then
-  PKG_PATCH_DIRS+=" opengl"
+if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  PKG_CMAKE_OPTS_TARGET+=" -DUSE_GLES2=ON -DUSE_OPENGL=OFF"
+elif [ "${OPENGL_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGL} glu libglvnd"
+  PKG_CMAKE_OPTS_TARGET+=" -DUSE_OPENGL=ON -DUSE_GLES2=OFF"
 fi
 
-pre_configure_target() {
-  export LDFLAGS="${LDFLAGS} -logg"
+if [ "${VULKAN_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${VULKAN}"
+  PKG_CMAKE_OPTS_TARGET+=" -DUSE_VULKAN=ON"
+else
+  PKG_CMAKE_OPTS_TARGET+=" -DUSE_VULKAN=OFF"
+fi
 
-  cd ${PKG_BUILD}
-  export SYSROOT_PREFIX=${SYSROOT_PREFIX}
-  AMIBERRY_PLATFORM="PLATFORM=${DEVICE}"
-
-  sed -i "s|AS     = as|AS     \?= as|" Makefile
-  PKG_MAKE_OPTS_TARGET+="${AMIBERRY_PLATFORM} all SDL_CONFIG=${SYSROOT_PREFIX}/usr/bin/sdl2-config"
-}
+PKG_CMAKE_OPTS_TARGET+=" -DCMAKE_BUILD_TYPE=Release \
+                         -DUSE_DBUS=OFF \
+                         -DUSE_GPIOD=OFF \
+                         -DUSE_PORTMIDI=OFF \
+                         -DUSE_UAENET_PCAP=OFF \
+                         -DBUNDLE_SDL=OFF"
 
 makeinstall_target() {
-  # Create directories
   mkdir -p ${INSTALL}/usr/bin
   mkdir -p ${INSTALL}/usr/lib
   mkdir -p ${INSTALL}/usr/config/amiberry
-  # mkdir -p ${INSTALL}/usr/config/amiberry/controller
 
-  # Copy ressources
+  # Copy binary
+  if [ -f ${PKG_BUILD}/.${TARGET_NAME}/amiberry ]; then
+    cp ${PKG_BUILD}/.${TARGET_NAME}/amiberry ${INSTALL}/usr/bin/amiberry
+  elif [ -f ${PKG_BUILD}/.${TARGET_NAME}/bin/amiberry ]; then
+    cp ${PKG_BUILD}/.${TARGET_NAME}/bin/amiberry ${INSTALL}/usr/bin/amiberry
+  else
+    AMIFOUND=$(find ${PKG_BUILD}/.${TARGET_NAME} -name "amiberry" -type f -executable | head -1)
+    if [ -n "${AMIFOUND}" ]; then
+      cp "${AMIFOUND}" ${INSTALL}/usr/bin/amiberry
+    else
+      echo "ERROR: amiberry binary not found"
+      exit 1
+    fi
+  fi
+
+  # Copy config and resources
   cp -ra ${PKG_DIR}/config/*           ${INSTALL}/usr/config/amiberry/
-  cp -a data                          ${INSTALL}/usr/config/amiberry/
-  cp -a savestates                    ${INSTALL}/usr/config/amiberry/
-  cp -a screenshots                   ${INSTALL}/usr/config/amiberry/
-  cp -a whdboot                       ${INSTALL}/usr/config/amiberry/
+  if [ -d ${PKG_BUILD}/data ]; then
+    cp -a ${PKG_BUILD}/data            ${INSTALL}/usr/config/amiberry/
+  fi
+  if [ -d ${PKG_BUILD}/savestates ]; then
+    cp -a ${PKG_BUILD}/savestates      ${INSTALL}/usr/config/amiberry/
+  fi
+  if [ -d ${PKG_BUILD}/screenshots ]; then
+    cp -a ${PKG_BUILD}/screenshots     ${INSTALL}/usr/config/amiberry/
+  fi
+  if [ -d ${PKG_BUILD}/whdboot ]; then
+    cp -a ${PKG_BUILD}/whdboot         ${INSTALL}/usr/config/amiberry/
+  fi
   ln -s /storage/roms/bios            ${INSTALL}/usr/config/amiberry/kickstarts
 
   # Create links to Retroarch controller files
   ln -s "/usr/share/libretro/autoconfig" "${INSTALL}/usr/config/amiberry/controller"
 
-  # Copy binary, scripts & link libcapsimg
-  cp -a amiberry* ${INSTALL}/usr/bin/amiberry
+  # Copy scripts & link libcapsimg
   cp -a ${PKG_DIR}/scripts/*          ${INSTALL}/usr/bin
   ln -sf /usr/lib/libcapsimage.so.5.1 ${INSTALL}/usr/config/amiberry/capsimg.so
 }
