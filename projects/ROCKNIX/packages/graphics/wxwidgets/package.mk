23abb811dd532494ff0fe01fe6388a01e933b5bd
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-04-03 12:51:44 +1300

    wxwidgets: bump to 3.3.2 (required by upstream CEMU HEAD)
    CEMU upstream updated wxWidgets dependency from 3.2.x to 3.3.x in
    commit a04eb538 ('vcpkg: Update wxWidgets to 3.3.2'). Only CEMU
    depends on wxWidgets in the ROCKNIX tree.

--- projects/ROCKNIX/packages/graphics/wxwidgets/package.mk
+++ projects/ROCKNIX/packages/graphics/wxwidgets/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="wxwidgets"
-PKG_VERSION="b97aee4"
+PKG_VERSION="db1005d"
 PKG_LICENSE="wxWindows Library Licence"
 PKG_SITE="https://github.com/wxWidgets/wxWidgets"
 PKG_URL="${PKG_SITE}.git"
-PKG_GIT_CLONE_BRANCH="3.2"
+PKG_GIT_CLONE_BRANCH="3.3-release"
 PKG_DEPENDS_TARGET="toolchain zlib libpng libjpeg-turbo gdk-pixbuf gtk3 libaio"
 PKG_LONGDESC="wxWidgets is a free and open source cross-platform C++ framework for writing advanced GUI applications using native controls."
 
