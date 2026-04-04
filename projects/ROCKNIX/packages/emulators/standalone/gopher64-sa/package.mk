74481e746bdbb86d9d49b0d5698e4c26e08f14cd
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-04-04 20:14:50 +1300

    emulators: bump standalone to latest; ppsspp v1.20.2 + clock speed feature; dolphin-sa wayland refactor

--- projects/ROCKNIX/packages/emulators/standalone/gopher64-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/gopher64-sa/package.mk
@@ -3,7 +3,7 @@
 
 PKG_NAME="gopher64-sa"
 PKG_LICENSE="GPLv3"
-PKG_VERSION="acf5e08b97d8526e7f25578bdaa202557c42c200"
+PKG_VERSION="1c6e23b7f2e9126d9433f42edc62427c1b46e187"
 PKG_SITE="https://github.com/gopher64/gopher64"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain SDL3 cargo:host cargo rust mesa libXss ${VULKAN}"
