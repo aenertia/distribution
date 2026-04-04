1ef96bad1e851ede023db423c918fc8f2899b0f5
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-04-04 20:15:10 +1300

    wayvnc: add VNC server for sway (wayvnc + neatvnc + aml + jansson); fix service crash + ordering

--- projects/ROCKNIX/packages/ui/emulationstation/package.mk
+++ projects/ROCKNIX/packages/ui/emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="1fdae88a23904b43d7f17b5e4685609b4f6d785b"
+PKG_VERSION="36970097c50613760da3cac4e81b0dfa61e693c7"
 PKG_GIT_CLONE_BRANCH="rxnm-integration"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/aenertia/emulationstation-next"
