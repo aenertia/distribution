4f97f0568733000ac41a3461a68d41737d87b07a
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-04-04 19:56:39 +1300

    wayland: migrate to wlroots 0.20.0 + sway 1.12-rc1

--- projects/ROCKNIX/packages/wayland/lib/wlroots/package.mk
+++ projects/ROCKNIX/packages/wayland/lib/wlroots/package.mk
@@ -2,32 +2,20 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wlroots"
+PKG_VERSION="0.20.0"
 PKG_LICENSE="MIT"
 PKG_SITE="https://gitlab.freedesktop.org/wlroots/wlroots/"
-PKG_DEPENDS_TARGET="toolchain libinput libxkbcommon pixman libdrm wayland wayland-protocols seatd xwayland hwdata libxcb xcb-util-wm"
+PKG_URL="${PKG_SITE}/-/archive/${PKG_VERSION}/wlroots-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libinput libxkbcommon pixman libdrm libdisplay-info wayland wayland-protocols seatd xwayland hwdata libxcb xcb-util-wm"
 PKG_LONGDESC="A modular Wayland compositor library"
 PKG_TOOLCHAIN="meson"
 
+# libmali zero-stride workaround for ARM Mali blob users
 case ${DEVICE} in
-  RK3326|RK3566|S922X)
-    # despite the '-rk' indication this is a wlroots version with libmali hacks
-    PKG_VERSION="0.19.3-rk"
-    PKG_SHA256="5385dc105f2c4c5fe3157e0b0299d6508765086d605fe4efe3ae437d4f18a5d9"
+  RK3326|RK3566|S922X|RK3588)
     PKG_PATCH_DIRS+=" libmali"
-    PKG_URL="https://github.com/rocknix/rockchip-wlroots/archive/refs/tags/${PKG_VERSION}.tar.gz"
-  ;;
-  RK3588|SDM845)
-    PKG_VERSION="0.17.4-rk"
-    PKG_SHA256="e9e1e14966c6272ca595307fa817fd0fefae96b13fe36c8084b3a7a55fed20d1"
-    PKG_URL="https://github.com/rocknix/rockchip-wlroots/archive/refs/tags/${PKG_VERSION}.tar.gz"
-  ;;
-  *)
-    PKG_VERSION="0.19.3"
-    PKG_SHA256="a6ff89b64ea15e424d1b0db4a22145fccf5ec2ff2e7b8af0fa35e2ac8975986f"
-    PKG_URL="${PKG_SITE}/-/archive/${PKG_VERSION}/wlroots-${PKG_VERSION}.tar.gz"
-  ;;
+    ;;
 esac
-
 
 configure_package() {
   # OpenGLES Support
@@ -35,17 +23,12 @@ configure_package() {
     PKG_DEPENDS_TARGET+=" ${OPENGLES}"
   fi
 }
-# to enable xwayland package: https://gitlab.freedesktop.org/xorg/lib/libxcb-wm/-/tree/master/icccm?ref_type=heads
+
 PKG_MESON_OPTS_TARGET="-Dxcb-errors=disabled \
                        -Dxwayland=enabled \
                        -Dexamples=false \
                        -Drenderers=gles2 \
                        -Dbackends=drm,libinput"
-
-unpack() {
-  mkdir -p ${PKG_BUILD}
-  tar --strip-components=1 -xf ${SOURCES}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz -C ${PKG_BUILD}
-}
 
 pre_configure_target() {
   # wlroots does not build without -Wno flags as all warnings being treated as errors
