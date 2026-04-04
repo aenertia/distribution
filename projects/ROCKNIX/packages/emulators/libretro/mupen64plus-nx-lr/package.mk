88abca4019963bb2022c0f84c89fc7b306d458e6
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-04-04 20:14:50 +1300

    emulators: bump libretro cores to latest upstream

--- projects/ROCKNIX/packages/emulators/libretro/mupen64plus-nx-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/mupen64plus-nx-lr/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="mupen64plus-nx-lr"
-PKG_VERSION="7c7f11061f29b2ccd5d0952e2373ae5b55cfea8f"
+PKG_VERSION="3a676196500545b637b83cb19fb393d2359e1f9d"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mupen64plus-libretro-nx"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
--- projects/ROCKNIX/packages/emulators/libretro/nestopia-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/nestopia-lr/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="nestopia-lr"
-PKG_VERSION="5deada54077fae87e2873f5ad9ef77e3ab7af5e1"
+PKG_VERSION="c0ae3bcbe78a1a21a20384b96b70774cc165d2c2"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/nestopia"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
