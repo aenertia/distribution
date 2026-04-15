58cc5afb3bd4866ba0e54b05b37bdf1e2d5538c6
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-04-15 11:55:46 +0000

    chore(cemu-sa): update PKG_VERSION to rebased rxnext HEAD
    Rebase rxnext (10 ROCKNIX patches) onto upstream origin/main 9e28d6d.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- projects/ROCKNIX/packages/emulators/standalone/cemu-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/cemu-sa/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="cemu-sa"
-PKG_VERSION="fe59926689a3fcf3040166b0b4a591e39bcded70" # last-known-tag: v2.6
+PKG_VERSION="94213643a2b55c5fa7f5deb0a7b0f5090faa9be7" # last-known-tag: v2.6
 PKG_LICENSE="MPL-2.0"
 PKG_SITE="https://github.com/cemu-project/Cemu"
 # Local clone for dev iteration (rxnext branch with ROCKNIX patches)
